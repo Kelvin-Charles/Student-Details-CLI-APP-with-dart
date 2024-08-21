e2867300eca3d2e6128b69dd9e85419f91e36658
Kelvin-Charles <84671154+Kelvin-Charles@users.noreply.github.com>
2024-08-21 09:04:56 +0300

    Create mkuu.dart
    creating admin view and activities....

---
+++ lib/mkuu.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'dart:io';
+import 'RegistrarRegistration.dart';
+import 'UserRegister.dart';
+
+import 'admin.dart';
+
+Future mkuuAct() async {
+  print("What do u want? ");
+  print("1. Register Registrar");
+  print("2. Register Normal User");
+  stdout.write("press 1 to Registrar or 2 for Normal user");
+  var userInput = stdin.readLineSync();
+  if (userInput == "1") {
+    registrarRegister();
+  } else if (userInput == "2") {
+    userRegister();
+  }
+}
+
+Future mkuu() async {
+ 
+  mkuuAct();
+}
